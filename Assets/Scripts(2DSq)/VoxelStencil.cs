7c977871804724ef1a3c3b30aca22b83ea1a30d8
Wyndfall <aldenmauro@suddenlink.com>
2020-12-10 14:23:35 -0800

    First Stable version of Marching Squares and Cellular Automata

---
+++ Assets/Scripts(2DSq)/VoxelStencil.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoxelStencil : MonoBehaviour
+{
+	protected bool fillType;
+	protected int centerX, centerY, radius;
+	public virtual void Initialize (bool fillType, int radius)
+    {
+		this.fillType = fillType;
+		this.radius = radius;
+    }
+
+	public virtual void SetCenter (int x, int y)
+    {
+		centerX = x;
+		centerY = y;
+    }
+
+	public virtual bool Apply(int x, int y, bool voxel)
+	{
+		return fillType;
+	}
+	//Which voxels it applies to
+	public int XStart
+	{
+		get
+		{
+			return centerX - radius;
+		}
+	}
+
+	public int XEnd
+	{
+		get
+		{
+			return centerX + radius;
+		}
+	}
+
+	public int YStart
+	{
+		get
+		{
+			return centerY - radius;
+		}
+	}
+
+	public int YEnd
+	{
+		get
+		{
+			return centerY + radius;
+		}
+	}
+
+}
